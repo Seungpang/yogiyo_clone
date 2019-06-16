56e2b1959cc10eecbd868c3933488e0f05cb26c0
seungpang <obey1342@gmail.com>
2019-06-17 02:05:45 +0900

    shop 모델 추가 및 마이그레이션

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    icon = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, db_index=True)
+
+class Shop(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    desc = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, db_index=True)
+
+class Review(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(blank=True)
+    rating = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    massage = models.TextField()
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    desc = models.TextField(blank=True)
+    amount = models.PositiveIntegerField()
+    photo = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, db_index=True)     
+
+
+#class Order(models.Model):
+ #   pass      
