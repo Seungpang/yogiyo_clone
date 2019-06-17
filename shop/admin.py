957244a297603b7db18ffd2b8b09b3ad4dc77156
seungpang <obey1342@gmail.com>
2019-06-18 05:10:25 +0900

    Shop admin 수정

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,4 @@
+from urllib.parse import quote
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import Category, Shop, Item
@@ -17,7 +18,15 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'address_link')
+    
+    def address_link(self, shop):
+        if shop.address:
+            url = 'https://map.naver.com/?query=' + quote(shop.address)
+            return mark_safe('<a href="{}" target="_blank">{}</a>'.format(url, shop.address))
+        return None
+    address_link.short_description = '주소 (네이버 지도)'
+
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
+from jsonfield import JSONField
 
 
 class Category(models.Model):
@@ -15,11 +16,18 @@ class Shop(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     desc = models.TextField(blank=True)
+    latlng = models.CharField(max_length=100, blank=True) ##Geojango 활용해서 바꿔보기 나중에일!
     photo = models.ImageField(blank=True)
     is_public = models.BooleanField(default=False, db_index=True)
-    
+    meta = JSONField()   #NoSQL적 특성
+                        #PostgreSQL의 JSONField와 다르다.
+
     def __str__(self):
         return self.name
+
+    @property
+    def address(self):
+        return self.meta.get('address')
 
 class Review(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
@@ -38,6 +46,7 @@ class Item(models.Model):
     amount = models.PositiveIntegerField()
     photo = models.ImageField(blank=True)
     is_public = models.BooleanField(default=False, db_index=True)     
+    meta = JSONField()
 
     def __str__(self):
         return self.name
